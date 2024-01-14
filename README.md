fb65aabe433549cb5d095fbabfd565302faa6e9d
Jonatan Adão <jonatan.adao.oliveira@gmail.com>
2024-01-14 14:55:59 -0300

    Adcionando 3 passo no README.me e Ciando janela no relogio.py

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 - Iniciando script 
 - 1° Passo = Importar as bibliotecas Tkinter.
 - 2° Passo = Definindo algumas cores para o projeto.
+- 3° Passo = criando e configurando Janela com Tkinter.
+    -   Dentro de passo 3 vou criar e configurar o frame da janela 
+
---
+++ relogio.py
@@ -0,0 +1,33 @@
+# Importando biblioteca Tkinter
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+
+
+#_______ CORES _________
+
+cor0 = '#ffffff' # branco
+cor1 = '#333333' # cinza
+cor2 = '#fcc058' # laranja
+cor3 = '#fff873' # amarelo
+cor4 = '#3297a8' # azul
+cor5 = '#fcc058' # laranja
+cor6 = '#e85151' # vermelha
+cor7 = '#34eb3d' # verde
+fundo = '#3b3b3b'
+
+'''____Criação e configuração de janela____'''
+
+
+janela = Tk()
+janela.title('RELOGIO')
+janela.geometry('280x200')
+janela.configure(bg= fundo)
+
+
+
+
+
+
+janela.mainloop()
