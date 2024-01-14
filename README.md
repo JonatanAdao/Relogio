e20a73fd292fbcfad5345c460c461ebbb92c2140
Jonatan Adão <jonatan.adao.oliveira@gmail.com>
2024-01-14 17:06:35 -0300

    Atualização de README.md e Adição de Data e hora no script

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Criando Relogio em Python 
-- Iniciando script 
+- Iniciando script.
 - 1° Passo = Importar as bibliotecas Tkinter.
 - 2° Passo = Definindo algumas cores para o projeto.
 - 3° Passo = criando e configurando Janela com Tkinter.
-    -   Dentro de passo 3 vou criar e configurar o frame da janela 
+    -   Dentro de passo 3 vou criar e configurar o frame da janela .
+    -   Adcionando biblioteca Datetime no arquivo Relogio.py.
+    -   Incrementação de data (Dia, Mês, ano) e Horario( Hora, Minutos, Seundos).
 
--- relogio.py
+++ relogio.py
@@ -3,7 +3,19 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 
+# importando biblioteca datetime
+from datetime import datetime
 
+
+agora = datetime.now()
+dia = agora.day
+mes = agora.month
+ano = agora.year
+div = '/'
+hora = agora.hour
+minutos = agora.minute
+segundos = agora.second
+div_1 = ':'
 
 #_______ CORES _________
 
@@ -25,9 +37,21 @@ janela.title('RELOGIO')
 janela.geometry('280x200')
 janela.configure(bg= fundo)
 
+frame_unico = Frame(janela, width= 280, height= 200, bg= cor1, relief= 'flat')
+frame_unico.grid(row= 0,column= 0, sticky= NW)
+
+data =  Label(frame_unico, text='Data :', height= 1, anchor='center', font=('Ivy 20 bold'), bg=cor1, fg= cor0)
+data.place(x= 20, y= 20)
+
+data_d_m_a = Label(frame_unico, text= [dia, div, mes, div, ano], height= 1, anchor='center', font=('Ivy 20 bold'), bg= cor1, fg= cor4)
+data_d_m_a.place(x= 100, y= 20)
 
 
+horario = Label(frame_unico, text= [hora, div_1, minutos,div_1, segundos], height= 2, anchor='center', font=('Ivy 40 bold'), bg= cor1, fg= cor6)
+horario.place(x= 5, y= 50)
 
+print(agora)
+print(dia,mes,ano)
 
 
 janela.mainloop()
